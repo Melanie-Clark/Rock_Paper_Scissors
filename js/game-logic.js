af7f1d98ce24fcfbe100b2b9c38e07b5d3f3a1f7
Melanie Clark <melaniefclark@gmail.com>
2026-01-03 17:33:18 +0000

    game-logic.js player move and type global variables added

--- js/game-logic.js
+++ js/game-logic.js
@@ -1,2 +1,19 @@
-// All code should be written in this file.
+// ----Player One Moves----
+let playerOneMoveOneType;
+let playerOneMoveOneValue;
 
+let playerOneMoveTwoType;
+let playerOneMoveTwoValue;
+
+let playerOneMoveThreeType;
+let playerOneMoveThreeValue;
+
+// ----Player Two Moves----
+let playerTwoMoveOneType;
+let playerTwoMoveOneValue;
+
+let playerTwoMoveTwoType;
+let playerTwoMoveTwoValue;
+
+let playerTwoMoveThreeType;
+let playerTwoMoveThreeValue;
