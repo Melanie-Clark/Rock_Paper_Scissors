3e0baece9973fbac29cdece1d05f9d05be2d5f00
Melanie Clark <melaniefclark@gmail.com>
2026-01-04 16:36:17 +0000

    getRoundWinner() player move variables added and getPlayerMove() moved from view-logic.js

--- js/game-logic.js
+++ js/game-logic.js
@@ -38,7 +38,11 @@ const setPlayerMoves = (player, moveOneType, moveOneValue, moveTwoType, moveTwoV
 };
 
 const getRoundWinner = (round) => {
+  const playerOneMove = getPlayerMove('Player One', round);
+  const playerTwoMove = getPlayerMove('Player Two', round);
+
   if (playerOneMove.type === playerTwoMove.type) {
+    console.log(playerOneMove, playerOneMove.type);
     if (playerOneMove.value > playerTwoMove.value) {
       return 'Player One';
     } else if (playerTwoMove.value > playerOneMove.value) {
@@ -56,6 +60,42 @@ const getRoundWinner = (round) => {
     return 'Player Two';
   }
 };
+function getPlayerMove(player, round) {
+    const move = {};
+    if (player === 'Player One') {
+      switch (round) {
+        case 1:
+          move.type = playerOneMoveOneType;
+          move.value = playerOneMoveOneValue;
+          break;
+        case 2:
+          move.type = playerOneMoveTwoType;
+          move.value = playerOneMoveTwoValue;
+          break;
+        case 3:
+          move.type = playerOneMoveThreeType;
+          move.value = playerOneMoveThreeValue;
+          break;
+      }
+    } else {
+      switch (round) {
+        case 1:
+          move.type = playerTwoMoveOneType;
+          move.value = playerTwoMoveOneValue;
+          break;
+        case 2:
+          move.type = playerTwoMoveTwoType;
+          move.value = playerTwoMoveTwoValue;
+          break;
+        case 3:
+          move.type = playerTwoMoveThreeType;
+          move.value = playerTwoMoveThreeValue;
+          break;
+      }
+    }
+    console.log(move);
+    return move;
+  };
 
 const getGameWinner = () => {
   const rounds = [1,2,3];
